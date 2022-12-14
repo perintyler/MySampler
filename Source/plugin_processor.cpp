8b8f2e1e9c9bafec6183b143e37e0141d8ff0a85
perintyler <perintyler@gmail.com>
2022-12-14 15:30:01 -0500

    plugin processor

---
+++ Source/plugin_processor.cpp
@@ -0,0 +1,67 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - Piano960 | VST3, AU - - - - - - - - - - - - - - - - - - - -
+// - - Created by Tyler Perin  - - - - - - - - - - - - - - - - - -
+// - - Copyright © 2022 Sound Voyager. All rights reserved.- - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+//   plugin_processor.cpp
+//   ~~~~~~~~~~~~~~~~~~~~
+//
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+#include "plugin_processor.h"
+#include "plugin_editor.h"
+
+Piano960Processor::Piano960Processor()
+    : AudioProcessor (BusesProperties().withInput("Input",  juce::AudioChannelSet::stereo(), true)
+                                       .withOutput("Output", juce::AudioChannelSet::stereo(), true))
+{
+    for (auto i = 0; i < NUM_VOICES; ++i) {
+        synthesiser.addVoice (new juce::SamplerVoice());
+    }
+    randomize_samples();
+}
+
+void Piano960Processor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    midiCollector.reset (sampleRate);
+    synthesiser.setCurrentPlaybackSampleRate (sampleRate);
+}
+
+bool Piano960Processor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+    return layouts.getMainOutputChannelSet() == juce::AudioChannelSet::mono()
+        && layouts.getMainOutputChannelSet() == juce::AudioChannelSet::stereo();
+}
+
+void Piano960Processor::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals; // What is this?
+    
+    // TODO: investigate if i actually need this (the plugin only recieves midi date)
+    // clear output channels that didn't contain input data since
+    // they aren't guaranteed to be empty (they may contain garbage)
+    for (auto i = getTotalNumInputChannels(); i < getTotalNumOutputChannels(); ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+    
+    midiCollector.removeNextBlockOfMessages (midiMessages, buffer.getNumSamples());
+    keyboardState.processNextMidiBuffer (midiMessages, 0, buffer.getNumSamples(), true);
+    synthesiser.renderNextBlock (buffer, midiMessages, 0, buffer.getNumSamples());
+}
+
+void Piano960Processor::randomize_samples()
+{
+    synthesiser.clearSounds();
+    for (int keyNumber = piano::C2; keyNumber < piano::C7; ++keyNumber)
+        synthesiser.addSound(getRandomSamplerSound(keyNumber));
+}
+
+juce::AudioProcessorEditor* Piano960Processor::createEditor()
+{
+    return new Piano960Editor (*this);
+}
+
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new Piano960Processor();
+}
---
+++ Source/plugin_processor.h
@@ -0,0 +1,86 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - Piano960 | VST3, AU - - - - - - - - - - - - - - - - - - - -
+// - - Created by Tyler Perin  - - - - - - - - - - - - - - - - - -
+// - - Copyright © 2022 Sound Voyager. All rights reserved.- - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+//   plugin_processor.h
+//   ~~~~~~~~~~~~~~~~~~
+//
+// This file declares the `Piano960Processor` class, which is
+// responsible for processing MIDI input and producing audio
+// output.
+//
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_audio_devices/juce_audio_devices.h>
+
+#include "piano.h"
+#include "samples.h"
+
+const int NUM_VOICES = 8;
+
+const int FIRST_KEY = piano::C1;
+
+const int LAST_KEY = piano::C7;
+
+class Piano960Processor: public juce::AudioProcessor {
+public:
+
+    Piano960Processor();
+    
+    ~Piano960Processor() override = default;
+    
+    void prepareToPlay(double sampleRate, int samplesPerBlock) override;
+    
+    void releaseResources() override {}
+
+    bool isBusesLayoutSupported(const BusesLayout& layouts) const override;
+
+    void processBlock(juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    void randomize_samples();
+    
+    juce::AudioProcessorEditor* createEditor() override;
+
+    bool hasEditor() const override { return true; }
+
+    const juce::String getName() const override { return JucePlugin_Name; }
+
+    bool acceptsMidi() const override { return true; }
+    
+    bool producesMidi() const override { return false; }
+    
+    bool isMidiEffect() const override { return false; }
+    
+    double getTailLengthSeconds() const override { return 0.0; }
+
+    int getNumPrograms() override { return 1; }
+    
+    int getCurrentProgram() override { return 0; }
+    
+    void setCurrentProgram(int index) override {}
+    
+    const juce::String getProgramName(int index) override { return {}; }
+    
+    void changeProgramName(int index, const juce::String& newName) override {}
+
+    void getStateInformation(juce::MemoryBlock&) override {}
+    
+    void setStateInformation(const void* data, int sizeInBytes) override {}
+
+    juce::MidiKeyboardState& getKeyboardState() { return keyboardState; }
+
+private:
+    
+    juce::MidiKeyboardState keyboardState;
+
+    juce::MidiMessageCollector midiCollector;
+
+    juce::Synthesiser synthesiser;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Piano960Processor)
+};
