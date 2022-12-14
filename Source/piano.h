d7d06705804d069e35b0ad35ef221a0889baa7f9
perintyler <perintyler@gmail.com>
2022-12-14 15:31:42 -0500

    note numbers

---
+++ Source/piano.h
@@ -0,0 +1,38 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - Piano960 | VST3, AU - - - - - - - - - - - - - - - - - - - -
+// - - Created by Tyler Perin  - - - - - - - - - - - - - - - - - -
+// - - Copyright © 2022 Sound Voyager. All rights reserved.- - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+//   piano.h
+//   ~~~~~~~
+//
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+#pragma once
+
+namespace piano {
+
+    const int OCTAVE_SIZE = 12;
+    
+    const int C0 = 12;
+    
+    const int G0 = C0 + 7;
+
+    const int C1 = C0 + OCTAVE_SIZE;
+
+    const int C2 = C0 + 2*OCTAVE_SIZE;
+    
+    const int C7 = C0 + 6*OCTAVE_SIZE;
+
+    const int B8 = C0 + 7*OCTAVE_SIZE - 1;
+
+    const int C8 = C0 + 7*OCTAVE_SIZE;
+
+    const int G8 = G0 + 7*OCTAVE_SIZE;
+
+    float getFrequency(int keyNumber);
+
+    int getKeyNumber(float frequency);
+
+} // piano namespace
