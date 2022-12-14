1fb7a5d7ab0b93a537cee4e45d32b12836941223
perintyler <perintyler@gmail.com>
2022-12-14 15:30:08 -0500

    plugin editor

---
+++ Source/plugin_editor.cpp
@@ -0,0 +1,52 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - Piano960 | VST3, AU - - - - - - - - - - - - - - - - - - - -
+// - - Created by Tyler Perin  - - - - - - - - - - - - - - - - - -
+// - - Copyright © 2022 Sound Voyager. All rights reserved.- - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+//   plugin_editor.cpp
+//   ~~~~~~~~~~~~~~~~~
+//
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+#include "plugin_editor.h"
+
+Piano960Editor::Piano960Editor(Piano960Processor& processor)
+    : AudioProcessorEditor (&processor)
+    , audioProcessor       (processor)
+    , keyboardComponent    (audioProcessor.getKeyboardState(), juce::MidiKeyboardComponent::horizontalKeyboard)
+    , randomizeButton      (RANDOMIZE_BUTTON_LABEL)
+{
+    randomizeButton.onClick = [&]() { processor.randomize_samples(); };
+
+    addAndMakeVisible(randomizeButton);
+    addAndMakeVisible(keyboardComponent);
+        
+    setSize(1220, 160);
+}
+
+// render the UI
+void Piano960Editor::paint(juce::Graphics& g)
+{
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.setColour(BACKGROUND_COLOR);
+}
+
+// lay out the subcomponents
+void Piano960Editor::resized()
+{
+    int keyboardWidth = getWidth() - 20;
+    int keyboardHeight = getHeight() - 50;
+
+    int buttonHeight = 30;
+    int buttonWidth = keyboardWidth;
+    
+    int keyboardXCoord = LEFT_MARGIN_SIZE;
+    int keyboardYCoord = TOP_MARGIN_SIZE + buttonHeight + 5;
+    
+    int buttonXCoord = LEFT_MARGIN_SIZE;
+    int buttonYCoord = TOP_MARGIN_SIZE;
+
+    randomizeButton.setBounds(buttonXCoord, buttonYCoord, buttonWidth, buttonHeight);
+    keyboardComponent.setBounds(keyboardXCoord, keyboardYCoord, keyboardWidth, keyboardHeight);
+}
---
+++ Source/plugin_editor.h
@@ -0,0 +1,51 @@
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// - - Piano960 | VST3, AU - - - - - - - - - - - - - - - - - - - -
+// - - Created by Tyler Perin  - - - - - - - - - - - - - - - - - -
+// - - Copyright © 2022 Sound Voyager. All rights reserved.- - - -
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//
+//   plugin_editor.h
+//   ~~~~~~~~~~~~~~~
+//
+// This file declares the `Piano960Editor` class, which is
+// responsible for the plugin's user interface.
+//
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_graphics/juce_graphics.h>
+#include <juce_audio_utils/juce_audio_utils.h>
+
+#include "plugin_processor.h"
+
+const juce::Colour BACKGROUND_COLOR = juce::Colours::grey;
+
+const int LEFT_MARGIN_SIZE = 10; // pixels
+
+const int TOP_MARGIN_SIZE = 5; // pixels
+
+const juce::String RANDOMIZE_BUTTON_LABEL { "randomize" };
+
+class Piano960Editor: public juce::AudioProcessorEditor {
+public:
+    
+    Piano960Editor(Piano960Processor&);
+    
+    ~Piano960Editor() override = default;
+
+    void paint(juce::Graphics&) override;
+
+    void resized() override;
+    
+private:
+    
+    Piano960Processor& audioProcessor;
+    
+    juce::MidiKeyboardComponent keyboardComponent;
+    
+    juce::TextButton randomizeButton;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Piano960Editor)
+};
