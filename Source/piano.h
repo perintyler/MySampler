edf3d164c76a57580d3cfa926117e5ba7d97ee17
perintyler <perintyler@gmail.com>
2022-12-15 00:18:26 -0500

    define piano::C3

--- Source/piano.h
+++ Source/piano.h
@@ -19,9 +19,11 @@ namespace piano {
     
     const int G0 = C0 + 7;
 
-    const int C1 = C0 + OCTAVE_SIZE;
+    const int C1 = C0 + 1*OCTAVE_SIZE;
 
     const int C2 = C0 + 2*OCTAVE_SIZE;
+
+    const int C3 = C0 + 3*OCTAVE_SIZE;
     
     const int C7 = C0 + 6*OCTAVE_SIZE;
 
