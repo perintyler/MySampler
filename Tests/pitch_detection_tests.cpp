70c34790d9246e03fa6341f71b3ee703152d15e5
perintyler <perintyler@gmail.com>
2022-12-14 23:37:11 -0500

    pitch detection tests

--- Tests/pitch_detection_tests.cpp
+++ Tests/pitch_detection_tests.cpp
@@ -7,6 +7,9 @@
 //   pitch_detection_tests.cpp
 //   ~~~~~~~~~~~~~~~~~~~~~~~~~
 //
+// This file contains unit tests for the `PitchDetection`
+// translation unit.
+//
 // = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 #include <vector>
@@ -18,16 +21,14 @@
 #include <catch2/catch_test_macros.hpp>
 #include <juce_audio_formats/juce_audio_formats.h>
 
-#include "config.h"
 #include "pitch_detection.h"
+#include "piano.h"
+#include "paths.h"
 
-std::vector<float> get_test_audio(std::filesystem::path fileName, int bufferSize, int sampleRate)
+float get_frequency(std::filesystem::path fileName)
 {
-    std::cout << TEST_DATA_DIRECTORY << std::endl;
-
     std::filesystem::path filePath = std::filesystem::path { TEST_DATA_DIRECTORY } / std::filesystem::path { fileName };
     juce::File wavFile { filePath.string() };
-
 
     assert(wavFile.existsAsFile());
     juce::WavAudioFormat wavFormat;
@@ -36,34 +37,112 @@ std::vector<float> get_test_audio(std::filesystem::path fileName, int bufferSize
     );
 
     juce::AudioSampleBuffer audioBuffer;
+    int bufferSize = 8192; // std::min((int) audioReader->lengthInSamples, (int) (audioReader->sampleRate));
     audioBuffer.clear(); // Why again?
     audioBuffer.setSize(audioReader->numChannels, bufferSize);
     audioReader->read(&audioBuffer, 0, bufferSize, 0, true, true);
 
-    const float* bufferReadPointer = audioBuffer.getReadPointer(0);
+    return getFundementalFrequency(audioBuffer.getReadPointer(0), bufferSize, audioReader->sampleRate);
+}
 
-    std::vector<float> audio;
-    for (int i = 0; i < bufferSize; ++i) {
-        std::cout << i << std::endl;
-        audio.push_back(bufferReadPointer[i]);
-    }
-
-    return audio;
-  }
-
-TEST_CASE("G5 female vocal chop", "[pitch_detection]") 
+TEST_CASE("Female Vocal: G5", "[pitch_detection]") 
 {
-    int bufferSize = 1000;
-    int sampleRate = 44100;
-    int expectedNoteNumber = 79;
-    std::vector<float> audio = get_test_audio("G5-female-vocal-chop.wav", bufferSize, sampleRate);
-    float frequency = getFundementalFrequency(audio.data(), bufferSize, sampleRate);
+    float frequency = get_frequency("G5-female-vocal-chop.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(174.61 < frequency);
+    REQUIRE(frequency < 196.00);
+}
+
+TEST_CASE("Female Vocal: G#3", "[pitch_detection]") 
+{
+    float frequency = get_frequency("G#3-female-vocal-chop.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
     REQUIRE(739.99 < frequency);
     REQUIRE(frequency < 830.61);
 }
 
-TEST_CASE("Piano One-Shot", "[pitch_detection]") 
-{}
+TEST_CASE("Male Vocal: A4", "[pitch_detection]") 
+{
+    float frequency = get_frequency("A4-male-vocal-chop.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(415.30 < frequency);
+    REQUIRE(frequency < 466.16);
+}
 
-TEST_CASE("Guitar One-Shot", "[pitch_detection]") 
-{}
+TEST_CASE("Piano: C3", "[pitch_detection]")
+{
+    float frequency = get_frequency("C3-guitar-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(123.47 < frequency);
+    REQUIRE(frequency < 138.59);
+}
+
+TEST_CASE("Upright Bass: C2", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C2-uprite-bass-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(61.74 < frequency);
+    REQUIRE(frequency < 69.30);
+}
+
+TEST_CASE("Exchange Bass: C4", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C4-exchange-bass-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(246.94 < frequency);
+    REQUIRE(frequency < 277.18);
+}
+
+TEST_CASE("Acoustic Bass: C3", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C3-acoustic-bass-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(123.47 < frequency);
+    REQUIRE(frequency < 138.59);
+}
+
+TEST_CASE("Piano: C6", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C6-piano-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(987.77 < frequency);
+    REQUIRE(frequency < 1108.73);
+}
+
+TEST_CASE("Piano: G3", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C3-piano-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(185.00 < frequency);
+    REQUIRE(frequency < 207.65);
+}
+
+TEST_CASE("Guitar: A2", "[pitch_detection]") 
+{
+    float frequency = get_frequency("A2-guitar-oneshot.wav");
+    std::cout << piano::getKeyNumber(frequency) << std::endl;
+    REQUIRE(103.83 < frequency);
+    REQUIRE(frequency < 116.54);
+}
+
+TEST_CASE("Guitar: E4", "[pitch_detection]") 
+{
+    float frequency = get_frequency("E4-guitar-oneshot.wav");
+    REQUIRE(311.13 < frequency);
+    REQUIRE(frequency < 349.23);
+}
+
+
+TEST_CASE("Keyboard: C5", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C5-keyboard-oneshot.wav");
+    REQUIRE(493.88 < frequency);
+    REQUIRE(frequency < 554.37);
+}
+
+TEST_CASE("Keyboard: C6", "[pitch_detection]") 
+{
+    float frequency = get_frequency("C6-keyboard-oneshot.wav");
+    REQUIRE(987.77 < frequency);
+    REQUIRE(frequency < 1108.73);
+}
