bb3fea4e63ba626fcbcec72d83d9b687a3fe79fc
perintyler <perintyler@gmail.com>
2023-03-01 17:07:52 -0500

    no need to initialize GUI in main test

--- Tests/main_test.cpp
+++ Tests/main_test.cpp
@@ -1,13 +1,7 @@
-/* main_test.cpp */
-
-#define CATCH_CONFIG_RUNNER
-#include "<catch2/catch.hpp>"
-#include "<juce_events/juce_events.h>"
+#include <catch2/catch_session.hpp>
 
 int main(int argc, char* argv[])
 {
-    juce::initialiseJuce_GUI();
-    int result = Catch::Session().run(argc, argv);
-    juce::shutdownJuce_GUI();
-    return result;
+    Catch::Session session;
+    return session.run();
 }
