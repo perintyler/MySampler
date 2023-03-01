220b2fc2126d85dbecc95c255ff609466c4e5cb4
perintyler <perintyler@gmail.com>
2023-03-01 14:57:45 -0500

    create test stubs for app tests that need to be written

---
+++ Tests/app_tests.cpp
@@ -0,0 +1,28 @@
+/* editor_tests.cpp */
+
+#include <catch2/catch_test_macros.hpp>
+
+#include "audio_processor.h"
+#include "app.h"
+
+TEST("app: keyboard is sub-component") 
+{}
+
+TEST("app: randomize button is sub-component") 
+{}
+
+TEST("app: paints keyboard") 
+{}
+
+TEST("app: paints randomize button") 
+{}
+
+TEST("app: lays out keyboard") 
+{}
+
+TEST("app: lays out randomize button") 
+{}
+
+TEST("app: cleans up sub-components upon destuction") 
+{}
+
