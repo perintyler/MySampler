bb9cf30f5d37ec73f13db63f1cda21037c9719df
perintyler <perintyler@gmail.com>
2023-03-02 22:32:15 -0500

    function for loading the spice model

--- Source/pitch_detection_v2.cpp
+++ Source/pitch_detection_v2.cpp
@@ -1,5 +1,18 @@
 /* pitch_detection_v2.cpp */
 
+tensorflow::SavedModelBundle load_model()
+{
+    tensorflow::SavedModelBundle model;
+    tensorflow::Status status = tensorflow::LoadSavedModel(
+        tensorflow::SessionOptions(), 
+        tensorflow::RunOptions(), 
+        "/usr/local/include/Piano960/model/model.pb", 
+        {tensorflow::kSavedModelTagServe}, 
+        &model
+    );
+    return model;
+}
+
 float pitch_detection_v2::getFundementalFrequency(const float* signal, int bufferSize, int sampleRate)
 {
     return 0.0;
