4264899d0cc5e1955d1085104e033fdcd107b0fa
perintyler <perintyler@gmail.com>
2023-03-07 11:54:19 -0500

    declare SpiceModelError

--- Source/pitch_detection_v2.h
+++ Source/pitch_detection_v2.h
@@ -6,6 +6,10 @@
 
 namespace pitch_detection_v2 
 {
+    struct SpiceModelError : public std::exception {
+        const char * what () const throw ();
+    };
+
     bool model_is_loaded();
     void load_model();
     float getFundementalFrequency(juce::AudioBuffer<float>&, int sampleRate);
