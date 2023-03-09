cddfbae39f2f422c0e08ffe52a10ed7874a7cbaa
perintyler <perintyler@gmail.com>
2023-03-09 17:22:32 -0500

    create translation unit for spice pitch detection

---
+++ Source/pitch_detection/spice.h
@@ -0,0 +1,14 @@
+/* pitch_detection/spice.h */
+
+#pragma once
+
+#include <juce_audio_formats/juce_audio_formats.h>
+
+namespace pitch_detection
+{
+    bool model_is_loaded();
+
+    void load_model();
+
+    float getFundementalFrequency(juce::AudioBuffer<float>&, int sampleRate);
+}
