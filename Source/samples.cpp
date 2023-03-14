5db91de7598e1373096802d7f1f0143b81592c39
perintyler <perintyler@gmail.com>
2023-03-14 01:16:53 -0400

    move random file function to samples.cpp

--- Source/samples.cpp
+++ Source/samples.cpp
@@ -12,6 +12,7 @@
 #include <cmath>
 #include <string>
 #include <filesystem>
+#include <random>
 
 #include "samples.h"
 #include "logs.h"
@@ -24,30 +25,37 @@
   const std::string PATH_TO_SAMPLES_DIRECTORY { "/usr/local/include/Piano960/Resources" };
 #endif
 
-std::unique_ptr<juce::AudioFormatReader> createWAVReader(juce::File& wavFile)
-{
-    juce::WavAudioFormat wavFormat;
+static std::random_device randomDevice;
+static std::mt19937 numberGenerator(randomDevice()); // Mersenne Twister
+static std::uniform_real_distribution<double> uniformDistribution(0.0, 1.0); // unit interval uniform distribution
 
-    if (!wavFile.existsAsFile()) {
-        juce::String errorMessage = "wav file does not exist: " + wavFile.getFullPathName();
-        throw std::filesystem::filesystem_error(errorMessage.toStdString(), std::error_code());
-    } else if (wavFile.getFileExtension() != ".wav") {
-        juce::String errorMessage = "sample is not a wav file: " + wavFile.getFullPathName();
-        throw std::filesystem::filesystem_error(errorMessage.toStdString(), std::error_code());
+juce::String getPathToRandomSample()
+{
+    std::string path;
+    int numFilesTraversed = 1;
+    std::filesystem::recursive_directory_iterator fileIterator(PATH_TO_SAMPLES_DIRECTORY);
+    
+    for (const std::filesystem::directory_entry &entry: fileIterator) {
+        if (!std::filesystem::is_directory(entry)) {
+            if (uniformDistribution(numberGenerator) < (1.0 / numFilesTraversed)) {
+               path = entry.path().string();
+            }
+            numFilesTraversed++;
+        }
     }
 
-    return std::unique_ptr<juce::AudioFormatReader>(
-        wavFormat.createReaderFor(wavFile.createInputStream().release(), true)
-    );
+    return juce::String { juce::String };
 }
 
-juce::AudioSampleBuffer createAudioBuffer(std::unique_ptr<juce::AudioFormatReader>& audioReader, int bufferSize)
+void validateSample(juce::File& sample, juce::String pathToFile)
 {
-    juce::AudioSampleBuffer buffer;
-    bufferSize = std::min(bufferSize, (int) audioReader->lengthInSamples);
-    buffer.setSize(audioReader->numChannels, bufferSize);
-    audioReader->read(&buffer, 0, bufferSize, 0, true, true);
-    return buffer;
+    if (!sample.existsAsFile()) {
+        juce::String errorMessage = "wav file does not exist: " + pathToFile;
+        throw std::filesystem::filesystem_error(errorMessage.toStdString(), std::error_code());
+    } else if (sample.getFileExtension() != ".wav") {
+        juce::String errorMessage = "sample is not a wav file: " + pathToFile;
+        throw std::filesystem::filesystem_error(errorMessage.toStdString(), std::error_code());
+    }
 }
 
 /** Generates a random sample from the installed wav files. The sample will be transposed
@@ -57,21 +65,29 @@ juce::SamplerSound* getRandomSamplerSound(NoteID note)
 {
     juce::File randomSample;
     int rootNoteOfSample;
-    std::unique_ptr<juce::AudioFormatReader> audioReader;
     juce::String pathToFile;
+    juce::WavAudioFormat wavFormat;
+    std::unique_ptr<juce::AudioFormatReader> audioReader;
+    bool foundValidSample = false;
 
-    while (audioReader == nullptr) 
-    {
-        pathToFile = juce::String { getPathToRandomFile(PATH_TO_SAMPLES_DIRECTORY) };
+    while (!foundValidSample) {
+        pathToFile = getPathToRandomSample();
         juce::File randomSample(pathToFile);
-        audioReader = createWAVReader(randomSample);
+        validateSample(randomSample, pathToFile);
+
+        juce::AudioSampleBuffer buffer;
         int bufferSize = static_cast<int>(audioReader->lengthInSamples);
-        juce::AudioSampleBuffer buffer = createAudioBuffer(audioReader, bufferSize);
-        
+        buffer.setSize(audioReader->numChannels, bufferSize);
+
+        audioReader = std::unique_ptr<juce::AudioFormatReader>(
+            wavFormat.createReaderFor(randomSample.createInputStream().release(), true)
+        );
+        audioReader->read(&buffer, 0, bufferSize, 0, true, true);
+
         try {
             rootNoteOfSample = detectNote(buffer, audioReader->sampleRate);
-        } 
-        catch (FrequencyNotDetectedException) {
+            foundValidSample = true;
+        } catch (FrequencyNotDetectedException) {
             logs::newBadSample(pathToFile);
         }
     }
@@ -80,8 +96,7 @@ juce::SamplerSound* getRandomSamplerSound(NoteID note)
     keyRange.setRange(note, note+1, true);
     
     return new juce::SamplerSound(
-        pathToFile, *audioReader,
-        keyRange, rootNoteOfSample,
+        pathToFile, *audioReader, keyRange, rootNoteOfSample,
         ATTACK, RELEASE, MAX_SAMPLE_LENGTH
     );
 }
