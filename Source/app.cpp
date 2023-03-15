1b8cb63094566840fa4b78efba0a0ed34eb03b56
perintyler <perintyler@gmail.com>
2023-03-14 21:48:57 -0400

    remove unused constants

--- Source/app.cpp
+++ Source/app.cpp
@@ -11,12 +11,6 @@ const juce::Colour BACKGROUND_COLOR = juce::Colours::grey;
 
 const int HORIZONTAL_MARGIN_SIZE = 10; // pixels
 
-const int VERTICAL_MARGIN_SIZE = 5; // pixels
-
-const juce::String RANDOMIZE_BUTTON_LABEL { "randomize" };
-
-const juce::String SAVE_BUTTON_LABEL { "save" };
-
 const float PLUGIN_HEIGHT = 250.0;
 
 App::App(AudioProcessor& audioProcessor) 
@@ -34,29 +28,7 @@ App::App(AudioProcessor& audioProcessor)
             }
         }
     ))
-{  
-    // std::function<void()> onRandomizeButtonClicked = [this]() {
-    //     synthProcessor.randomizeSamples();
-    // };
-
-    // std::function<void()> onSaveButtonClicked = [this]() {
-    //     synthProcessor.randomizeSamples();
-    // };
-
-    // std::function<void(Note note)> onLockButtonClicked = [this](Note note) {
-    //     if (this->synthProcessor.isKeyLocked(note))
-    //         this->synthProcessor.unlockKey(note);
-    //     else
-    //         this->synthProcessor.lockKey(note);
-    // };
-
-    // view = std::make_unique<MainView>(
-    //     synthProcessor.getKeyboardState(), 
-    //     onRandomizeButtonClicked,
-    //     onSaveButtonClicked,
-    //     onLockButtonClicked
-    // );
-
+{
     setSize(view->getMinimumWidth() + 2*HORIZONTAL_MARGIN_SIZE, PLUGIN_HEIGHT);
 
     addAndMakeVisible(view.get());
@@ -64,19 +36,12 @@ App::App(AudioProcessor& audioProcessor)
     setResizable(false, false);
 }
 
-App::~App()
-{
-    // assert(view);
-    // removeChildComponent(view.get());
-    // view.release();
-}
-
-
 /** Render the UI
  **/
 void App::paint(juce::Graphics& g)
 {
     g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
+    g.setColour(BACKGROUND_COLOR);
 }
 
 /** Layout subcomponents
