d53cec1833d3d1fe1777c080695b07b36d36e591
perintyler <perintyler@gmail.com>
2023-03-14 20:14:06 -0400

    move sub-component tests to main_view tests

--- Tests/app_tests.cpp
+++ Tests/app_tests.cpp
@@ -1,36 +1,20 @@
 /* editor_tests.cpp */
 
 #include <catch2/catch_test_macros.hpp>
-#include <juce_graphics/juce_graphics.h>
 
 #include "audio_processor.h"
 #include "app.h"
 
 TEST_CASE("app tests") 
 {
-    // block messages from other threads for the duration of the test 
-    // we're not testing processing here 
-    juce::MessageManager::getInstance()->runDispatchLoop();
+    juce::ScopedJuceInitialiser_GUI libraryInitialiser;
+    const juce::MessageManagerLock mml;
 
-    AudioProcessor processor {};
-    App app { processor };
+    auto processor = AudioProcessor {};
+    processor.suspendProcessing(true);
+    auto app = App { processor };
 
     REQUIRE(app.isResizable() == false);
 
     REQUIRE(app.getBounds().getWidth() * app.getBounds().getHeight() > 0);
-
-    REQUIRE(app.findChildWithID("randomize-button")->isVisible());
-
-    REQUIRE(app.findChildWithID("keyboard")->isVisible());
-
-    REQUIRE(app.findChildWithID("randomize-button")->isVisible());
-
-    auto keyboardBounds = app.findChildWithID("keyboard")->getBounds();
-    REQUIRE(keyboardBounds.getWidth() * keyboardBounds.getHeight() > 0);
-
-    auto randomizeButtonBounds = app.findChildWithID("randomize-button")->getBounds();
-    REQUIRE(randomizeButtonBounds.getWidth() * randomizeButtonBounds.getHeight() > 0);
-
-    juce::MessageManager::getInstance()->stopDispatchLoop();
-    juce::MessageManager::deleteInstance();
 }
