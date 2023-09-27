a4f5441e04baed5197e04b661b5751cf443529a4
perintyler <perintyler@gmail.com>
2023-09-26 20:02:05 -0400

    random sampler class

--- Source/samples.h
+++ Source/samples.h
@@ -34,3 +34,36 @@ public:
 
   std::vector<std::pair<MidiNumber, Sample>> asVector();
 }
+
+/***
+ ** A synthesizer that generates a sound source for each key using a different 
+ ** random sample from Piano960's installed sample set. A key's sample can be
+ ** locked/unlocked (using the `lockKey` and `unlockKey` functions), meaning the
+ ** key's sound source won't be re-randomized every time `RandomSampler::randomize` 
+ ** is called.
+ ***/
+class RandomSampler
+{
+public:
+    juce::Synthesiser synthesiser; // make this private
+
+    RandomSampler(MidiNumber firstNote, MidiNumber lastNote);
+
+    bool isKeyLocked(MidiNumber);
+
+    void lockKey(MidiNumber);
+
+    void unlockKey(MidiNumber);
+
+    Sample& getSample(MidiNumber) const;
+
+    void randomize();
+
+    bool isReady();
+
+private:
+    MidiNumber firstNote;
+    MidiNumber lastNote;
+    SampleSet samples {};
+    std::map<Note, bool> lockedKeys {};
+}
