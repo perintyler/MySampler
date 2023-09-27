d2b6546280d77f10bba20c73e05f6217897cee89
perintyler <perintyler@gmail.com>
2023-09-27 12:06:53 -0400

    show example preset file in a comment

--- Source/presets.h
+++ Source/presets.h
@@ -1,4 +1,23 @@
-/*** Piano960 | presets.h */
+/*** Piano960 | presets.h ***/
+
+/***
+ * 
+ * The preset file is a JSON file where each key is the name if a preset. 
+ * Each value will be an object containing a MIDI number and a sample's file path.
+ *  
+ * ```json
+ * {
+ *    "my preset": {
+ *      23: {
+ *        "path": "path/to/sample/for/key/with/midi/number/23.wav",
+ *        "note": 42
+ *      },
+ *      ...
+ *    },
+ * }
+ * ```
+ * 
+ ***/
 
 #pragma once
 
