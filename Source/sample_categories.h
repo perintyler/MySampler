9f522a94408b636b7a2d2f39d908471fae6504c2
perintyler <perintyler@gmail.com>
2023-09-28 14:57:59 -0400

    sample category enum

---
+++ Source/sample_categories.h
@@ -0,0 +1,27 @@
+/*** Piano960 | sample_categories.h ***/
+
+#pragma once
+
+enum SampleCategory: int {
+    Piano = 0,
+    Cello,
+    Trombone,
+    Organ,
+    Woodwind,
+    Trumpet,
+    Glockenspiel,
+    SFX,
+    Guitar,
+    Pluck,
+    World,
+    Brass,
+    Bass,
+    Vocal,
+    Pad,
+    Synth,
+    Bell,
+    Voilin,
+    Arp,
+    Flute,
+    Keyboard
+};
