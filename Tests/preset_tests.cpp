1d2f913792892f92dd7b4ba89d9ecfbdba101aa8
perintyler <perintyler@gmail.com>
2023-09-29 19:53:51 -0400

    test get preset

--- Tests/preset_tests.cpp
+++ Tests/preset_tests.cpp
@@ -1,24 +1,36 @@
 /*** Piano960 Tests | presets_test.cpp ***/
 
 #include <algorithm>
+#include <filesystem>
 
 #include <catch2/catch_test_macros.hpp>
 
-#include "presets.h"
 #include "pitch_detection/notes.h"
+#include "presets.h"
+
+const std::vector<std::string> DEFAULT_PRESET_NAMES = { "default-preset-1", "default-preset-2", "default-preset-3" };
 
 TEST_CASE("Preset Test #1: get all preset names", "[presets]")
 {
-  std::vector<std::string> expected { "test-preset-1", "test-preset-2", "test-preset-3" };
-  std::vector<std::string> acutal= getPresetNames();
+    std::vector<std::string> actualNames = getPresetNames();
 
-  for (const std::string& presetName : expected)
-    REQUIRE(std::find(acutal.begin(), acutal.end(), presetName) != acutal.end());
+    for (const std::string& expectedName : DEFAULT_PRESET_NAMES) {
+        REQUIRE(std::find(actualNames.begin(), actualNames.end(), expectedName) != actualNames.end());
+    }
 }
 
 TEST_CASE("Preset Test #2: get preset", "[presets]")
 {
+    for (const std::string& presetName : DEFAULT_PRESET_NAMES) {
+        SampleSet samples = getSamplesForPreset(presetName);
+        REQUIRE(!samples.empty());
 
+        for (const auto& [note, sample] : samples) {
+            REQUIRE(isValidNote(note));
+            REQUIRE(isValidNote(sample.rootNote));
+            REQUIRE(std::filesystem::exists(sample.filepath));
+        }
+    }
 }
 
 TEST_CASE("Preset Test #3: add/delete preset", "[presets]")
