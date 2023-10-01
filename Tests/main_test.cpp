00b08fbfaf923662ddedc7d6a5ff516179cf4863
perintyler <perintyler@gmail.com>
2023-10-01 18:25:19 -0400

    define TESTMODE precompiler variable inside catch2 test runner file

--- Tests/main_test.cpp
+++ Tests/main_test.cpp
@@ -1,23 +1,18 @@
 #include <catch2/catch_session.hpp>
 
-#define TESTMODE
+#ifndef TESTMODE
+  #define TESTMODE
+#endif
 
 int main(int argc, char* argv[])
 {
-  Catch::Session session;
+    Catch::Session session;
 
-  int returnCode = session.applyCommandLine( argc, argv );
-  if( returnCode != 0 ) // Indicates a command line error
+    int returnCode = session.applyCommandLine( argc, argv );
+
+    if (returnCode != 0) { // invalid CLI args
         return returnCode;
-
-  // writing to session.configData() or session.Config() here
-  // overrides command line args
-  // only do this if you know you need to
-
-  int numFailed = session.run();
-
-  // numFailed is clamped to 255 as some unices only use the lower 8 bits.
-  // This clamping has already been applied, so just return it here
-  // You can also do any post run clean-up here
-  return numFailed;
+    } else {
+        return session.run();
+    }
 }
