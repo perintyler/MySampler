963b14e81c483707cf71481908137b948c6c17bf
perintyler <perintyler@gmail.com>
2023-10-03 15:28:00 -0400

    add new categories

--- Scripts/compile_samples.py
+++ Scripts/compile_samples.py
@@ -1,10 +1,4 @@
-"""
-  Piano960/Scripts/compile_samples.py
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-TODO: link files instead of copying files
-
-"""
+"""MySampler | Scripts/compile_samples.py"""
 
 import os
 import pathlib
@@ -17,11 +11,11 @@ import random
 from typing import Set
 import re
 
-NUM_SAMPLES = 500 # TODO: make this a command line argument
+NUM_SAMPLES_PER_CATEGORY = 100
 
-NUM_SAMPLES_PER_CATEGORY = 50
+PATH_TO_PIANO960_REPO = pathlib.Path(__file__).resolve().parent.parent.absolute()
 
-PATH_TO_PIANO960_REPO = pathlib.Path(__file__).parent.parent;
+ROOT_REPO = pathlib.Path(__file__).resolve().parent.parent.absolute()
 
 PATH_TO_LOG_FILE = PATH_TO_PIANO960_REPO.joinpath('sample-log.json').resolve()
 
@@ -30,10 +24,10 @@ SAMPLE_CATEGORIES = {
   'Keyboard': ['keys', 'key'],
   'Mallet': ['mallet', 'mallets'],
   'Vocal' : ['vocal', 'vocals', 'vo', 'choir', 'choirs'],
-  'Voilin': ['violin', 'violins', 'strings'],
+  'Strings': ['violin', 'violins', 'strings'],
   'Synth': ['synth', 'synths', 'leads', 'lead'],
   'Texture': ['texture', 'textures'],
-  'Bass': ['bass', 'basses'],
+  'Bass': ['bass', 'basses', '808'],
   'Pluck': ['plucked', 'pluck', 'plucks'],
   'World': ['world', 'ethnic'],
   'Bell': ['bell', 'bells'],
@@ -46,57 +40,61 @@ SAMPLE_CATEGORIES = {
   'Arp': ['arp', 'arps'],
   'Piano': ['piano', 'pianos', 'upright', 'grand'],
   'Organ': ['organ', 'organs'],
-  'Woodwind': ['woodwind', 'woodwinds'],
+  'Woodwind': ['woodwind', 'woodwinds', 'oboe', 'clarinet',],
+  'Oboe': ['oboe', 'oboes'],
+  'Clarinet': ['clarinet', 'clarinets'],
+  'Horn': ['horn', 'horns'],
+  'Saxophone': ['saxophone', 'saxophones', 'sax'],
   'Glockenspiel': ['glockenspiel', 'glockenspiels'],
+  'Tuba': ['tuba', 'tubas'],
   'Trumpet': ['trumpet', 'trumpets', 'horn', 'horns'],
   'Cello': ['cello', 'cellos'],
-  'Nature': ['space', 'nebula', 'jwst', 'water', 'leaves', 'wind', 'animal']
+  'Bassoon': ['bassoon', 'bassoons'],
+  'Nature': ['space', 'nebula', 'jwst', 'water', 'leaves', 'wind', 'animal'],
+  # TODO: break percussion up into multiple categories
+  'Percussion': ['percussion', 'hats', 'hihats', 'hihat', 'toms', 'kick', 'snare', 'crash'],
+  'Other': []
 }
 
 CATEGORIES_TO_AVOID = (
-  'accents',
+  'Accent',
+  'Other'
 )
 
-SAMPLE_PACKS_TO_IGNORE = (
-  'eclipse-oneshots-by-kindobands',
-)
-
-def is_sample_from_valid_pack(path_to_sample, sample_packs_directory):
-  relative_path = os.path.relpath(path_to_sample, sample_packs_directory)
-  for pack_to_ignore in SAMPLE_PACKS_TO_IGNORE:
-    if relative_path.startswith(pack_to_ignore):
-      return False
-  return True
+CATEGORY_SAMPLE_COUNTS = {category: 0 for category in SAMPLE_CATEGORIES.keys()}
 
 def generate_sample_paths(sample_packs_directory):
-  sample_paths = list(pathlib.Path(sample_packs_directory).glob(f"**/*.wav"))
-  assert len(sample_paths) > 0
+
+  sample_paths = []
+  for filetype in ['mp3', 'wav']:
+    sample_paths.extend(pathlib.Path(sample_packs_directory).glob(f"**/*.{filetype}"))
 
   while sample_paths:
     random_index = random.randint(0, len(sample_paths) - 1)
-    path_to_sample = sample_paths.pop(random_index)
-    if is_sample_from_valid_pack(path_to_sample, sample_packs_directory):
-      yield path_to_sample
+    yield sample_paths.pop(random_index)
 
 def get_categories(sample_path) -> list:
   path = str(sample_path).lower()
   for deliminator in ('/', ',', '.', '-', '_', '~'):
     path = path.replace(deliminator, ' ')
 
-  potential_category_identifiers = path.split(' ')
-  categorizations  = set()
-  for potential_category_id in potential_category_identifiers:
-    for category, category_identifiers in SAMPLE_CATEGORIES.items():
-      if potential_category_id in category_identifiers:
-        categorizations.add(category)
+  categories  = set()
 
-  return categorizations
+  for category, category_ids in SAMPLE_CATEGORIES.items():
+    if category in CATEGORIES_TO_AVOID:
+      continue
+    for category_identifier in category_ids:
+      if category_identifier in path:
+        categories.add(category)
+        break
 
-def valid_category_exists(categories):
-  for category in categories:
-    if category not in CATEGORIES_TO_AVOID:
-      return True
-  return False
+  if len(categories) == 0:
+    categories.add('Other')
+
+  return categories
+
+def getCategoryDirectory(category, compilation_directory):
+  return pathlib.Path(compilation_directory).joinpath(compilation_directory, category)
 
 def compile_samples(installation_directory, sample_packs_directory, print_details=False):
   """
@@ -104,24 +102,46 @@ def compile_samples(installation_directory, sample_packs_directory, print_detail
   installation directory. Each of the installed files will have a unique ID. The new file 
   names will follow this format: `<Sample-Number>.wav`.
   """
+  numSamplesConsidered = 0
   numCompiledSamples = 0
 
   for sample_number, path_to_sample in enumerate(generate_sample_paths(sample_packs_directory)):
-    if numCompiledSamples >= NUM_SAMPLES: break
+    numSamplesConsidered += 1
     categories = get_categories(path_to_sample)
-    if len(categories) == 0:
-      print(f'could not categorize {path_to_sample}')
-    elif valid_category_exists(categories):
-      relative_path = os.path.relpath(path_to_sample, sample_packs_directory)
-      path_components = relative_path.split(os.sep)
-      compiled_file_name = '.'.join(path_components)
-      for category in categories:
-        compiled_file_path = os.path.join(installation_directory, category, compiled_file_name)
+
+    if print_details:
+      print(f' | #{numSamplesConsidered} sample path: {path_to_sample.relative_to(sample_packs_directory)}')
+      print('\n'.join(f' | - {category}' for category in categories))
+      print()
+
+    for category in categories:
+      if CATEGORY_SAMPLE_COUNTS[category] <= NUM_SAMPLES_PER_CATEGORY:
+        relative_path = os.path.relpath(path_to_sample, sample_packs_directory)
+        path_components = relative_path.split(os.sep)
+        compiled_file_name = '.'.join(path_components)
+        compiled_file_path = pathlib.Path(installation_directory).joinpath(category, compiled_file_name)
+        
+        if print_details: 
+          print(f' <> Copying {category} Sample:')
+          print(f" <> - from : '{path_to_sample.relative_to(ROOT_REPO)}')")
+          print(f" <> -   to : '{compiled_file_path.relative_to(ROOT_REPO)}'")
+          print()
+
         shutil.copy2(path_to_sample, compiled_file_path)
-      if print_details: print(f'copying {path_to_sample} to {compiled_file_path}')
-      numCompiledSamples += 1
+        numCompiledSamples += 1
+        CATEGORY_SAMPLE_COUNTS[category] += 1
+        break
+
+  if print_details:
+    print('\n --- Category Counts ---')
+    print('\n'.join(f'   <> ({category}: {count})' for category, count in CATEGORY_SAMPLE_COUNTS.items()))
 
   print(f'succesfully compiled {numCompiledSamples} samples to {installation_directory}')
+
+def count_existing_samples(compiled_samples_directory):
+  for category in CATEGORY_SAMPLE_COUNTS.keys():
+    category_directory = os.path.join(compiled_samples_directory, category)
+    CATEGORY_SAMPLE_COUNTS[category] = len(os.listdir(category_directory))
 
 def main():
   argument_parser = argparse.ArgumentParser('Piano960 Sample Compilation Script')
@@ -169,6 +189,7 @@ def main():
   if not os.path.isdir(sample_packs_directory):
     print(f'sample pack path is not a directory ({sample_packs_directory})')
   else:
+    count_existing_samples(compilation_directory)
     compile_samples(compilation_directory, args.sample_packs, print_details=args.verbose)
 
   for category in SAMPLE_CATEGORIES.keys():
